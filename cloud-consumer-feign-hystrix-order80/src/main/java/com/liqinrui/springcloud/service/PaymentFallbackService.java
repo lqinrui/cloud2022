7f8a971adf76a1c4061dd42dff23b30967e0c12a
yuanlai <15235151477@163.com>
2022-04-17 19:59:57 +0800

    hystrix 服务降级修改PaymentFallbackService表情

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/liqinrui/springcloud/service/PaymentFallbackService.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/liqinrui/springcloud/service/PaymentFallbackService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class PaymentFallbackService implements PaymentHystrixService{
     @Override
     public String paymentInfo_OK(Integer id) {
-        return "-------PaymentFallbackService fall back-paymentInfo_OK , ^_^";
+        return "-------PaymentFallbackService fall back-paymentInfo_OK , o(╥﹏╥)o";
     }
 
     @Override

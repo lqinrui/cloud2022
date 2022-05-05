34b2b0a7251f755318dc61ba293bad091ea24fce
yuanlai <15235151477@163.com>
2022-05-05 23:24:35 +0800

    hystrix 服务熔断

--- cloud-provider-hystrix-payment8001/src/main/java/com/liqinrui/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/liqinrui/springcloud/service/PaymentService.java
@@ -1,14 +1,17 @@
 package com.liqinrui.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class PaymentService {
 
+    //======服务降级
     /**
      * 正常访问，肯定OK
      * @param id
@@ -37,5 +40,32 @@ public class PaymentService {
 
     public String paymentInfo_TimeOutHandle(Integer id){
         return "线程池："+Thread.currentThread().getName()+"  系统繁忙或者运行报错，请稍后再试,id："+id+"\to(╥﹏╥)o";
+    }
+
+
+    //============服务熔断
+
+    /**
+     *
+     * @param id
+     * @return
+     * @description  在时间窗口期10秒钟之内，10次请求次数失败率达到60%触发服务熔断
+     */
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"), // 是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),  // 请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),  // 时间窗口期
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60")  // 失败率达到多少跳闸
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        if (id < 0){
+            throw new RuntimeException("*********id 不能为负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+        return Thread.currentThread().getName()+"\t调用成功，流水号："+serialNumber;
+    }
+
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id){
+        return "id 不能为负数，请稍后再试，/(ㄒoㄒ)/~~   id："+id;
     }
 }

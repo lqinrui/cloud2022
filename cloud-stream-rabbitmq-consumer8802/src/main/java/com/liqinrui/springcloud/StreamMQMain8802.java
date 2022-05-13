8948492df6fe655b834a35195d948ff3b3728400
liqinrui <1758662160@qq.com>
2022-05-13 17:21:19 +0800

    stream消息驱动之消费者

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/liqinrui/springcloud/StreamMQMain8802.java
@@ -0,0 +1,17 @@
+package com.liqinrui.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lqinrui
+ * @date 2022-05-13 16:01:49
+ * @description
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/liqinrui/springcloud/StreamMQMain8803.java
@@ -0,0 +1,17 @@
+package com.liqinrui.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lqinrui
+ * @date 2022-05-13 16:01:49
+ * @description
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
